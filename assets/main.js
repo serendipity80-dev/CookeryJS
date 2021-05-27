3b58d17145902a967543d805a2eb5372fd54924b
serendipity80-dev <ljiljana.bogdanovic@yahoo.com>
2021-05-27 15:22:41 +0200

    finalized

---
+++ assets/main.js
@@ -0,0 +1,146 @@
+const menu = [
+    {
+        id:1,
+        title:"Garnele in ramsoße",
+        category:"dinner",
+        img:"../assets/img/02.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:2,
+        title:"Rumsteak",
+        category:"lunch",
+        img:"../assets/img/03.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:3,
+        title:"Pancakes",
+        category:"breakfast",
+        img:"../assets/img/04.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:4,
+        title:"Spagetti",
+        category:"lunch",
+        img:"../assets/img/05.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:5,
+        title:"Garnele in ramsoße",
+        category:"dinner",
+        img:"../assets/img/06.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:6,
+        title:"Garnele in ramsoße",
+        category:"breakfast",
+        img:"../assets/img/07.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:7,
+        title:"Garnele in ramsoße",
+        category:"lunch",
+        img:"../assets/img/08.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:8,
+        title:"Garnele in ramsoße",
+        category:"breakfast",
+        img:"../assets/img/09.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:9,
+        title:"Garnele in ramsoße",
+        category:"breakfast",
+        img:"../assets/img/10.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:10,
+        title:"Garnele in ramsoße",
+        category:"breakfast",
+        img:"../assets/img/11.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:11,
+        title:"Garnele in ramsoße",
+        category:"breakfast",
+        img:"../assets/img/12.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+    {
+        id:12,
+        title:"Garnele in ramsoße",
+        category:"breakfast",
+        img:"../assets/img/13.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate amet ut soluta reiciendis odio minus ipsam eos, veritatis eligendi.",
+    },
+];
+const cards = document.querySelector('.row');
+const btnContainer = document.querySelector('.btn-container');
+window.addEventListener('DOMContentLoaded', ()=>{
+ displayMenuItems(menu);
+ displayBtns();
+ 
+});
+
+
+const displayMenuItems = (menuItem) => {
+    let displayItems = menuItem.map( (item) =>{
+        return `
+          <div class="card">
+            <div class="card-image">
+              <img src=${item.img}>
+              <span class="card-title">${item.title}</span>
+             
+            </div>
+            <div class="card-content">
+              <p>${item.desc}</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Read More</a>
+            </div>
+          </div>
+      `;
+    });
+    displayItems = displayItems.join("");
+    cards.innerHTML = displayItems;
+}
+const displayBtns = () =>{
+    const categories = menu.reduce((values,item) =>{
+        if (!values.includes(item.category)) {
+            values.push(item.category);
+        }
+        return values;
+    }, ['all']
+    );
+    const categoryBtn = categories.map((category) =>{
+        return ` <button id="filter-btn" class="btn waves-effect waves-light btn-small" data-id=${category} type="button">${category}</button>`
+    }).join("");
+    btnContainer.innerHTML = categoryBtn;
+    const btns = document.querySelectorAll('#filter-btn');
+  // filter items
+  btns.forEach((btn) =>{
+    btn.addEventListener('click', (e) =>{
+       const category = e.currentTarget.dataset.id;
+       const menuCategory = menu.filter((menuItem) =>{
+           if (menuItem.category === category) {
+              return menuItem ;
+           }
+       });
+       if (category === 'all') {
+           displayMenuItems(menu);
+       }else{
+           displayMenuItems(menuCategory);
+       }
+    });
+});
+}
